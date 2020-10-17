aba5e948340be1bd6c88e362ff8419f6038bdcf1
Ahmed <ahmedafsal39@gmail.com>
2020-10-17 15:33:12 +0530

    complete: dynamic weather app

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 import requests
+from .models import City
 
 def index(request):
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8256a4112664476d12154f4697e5ca5'
-    city = 'trivandrum'
-    city_weather = requests.get(url.format(city)).json()
+    if request.method == 'POST':
+        city_name = request.POST['city_name'].upper()
+        if not City.objects.filter(name= city_name):
+            obj = City(name= city_name)
+            obj.save()
+            
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8256a4112664476d12154f4697e5ca5'
+        cities = City.objects.all()
+        weather_data = []
 
-    if url:
-        weather = {
+        try:
+            if cities:
+                for city in cities:
+                    city_weather = requests.get(url.format(city.name)).json()
+                    weather = {
 
-            'city': city,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
+                        'city': city.name,
+                        'temperature': city_weather['main']['temp'],
+                        'description': city_weather['weather'][0]['description'],
+                        'icon': city_weather['weather'][0]['icon']
+                    }
+
+                    weather_data.append(weather)
+        except KeyError:
+            print("key error")
+        except Exception as e:
+            print(e)
+        return render(request, 'index.html', {'weather_data': weather_data})
     else:
-        return HttpResponse('404 ! Page Not Found')
-
-    return render(request, 'index.html', {'weather': weather})
+        return render(request, 'index.html')
